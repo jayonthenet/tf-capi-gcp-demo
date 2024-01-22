30ea561210df71150105f03caa5750a07dfe599c
jayonthenet <jay@humanitec.com>
2024-01-22 09:42:59 +0100

    fix auth
    Signed-off-by: jayonthenet <jay@humanitec.com>

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 provider "kubernetes" {
   #config_path = "clustyconfig"
   host                   = var.host
-  token                  = var.token
-  cluster_ca_certificate = var.cluster_ca_certificate
+  token                  = base64decode(var.token)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
