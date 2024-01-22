39cb7cb0f5a496e32ad3ea0604f9411f83defd49
jayonthenet <jay@humanitec.com>
2024-01-22 09:26:46 +0100

    auth fix
    Signed-off-by: jayonthenet <jay@humanitec.com>

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "kubernetes" {
   #config_path = "clustyconfig"
-  host = "${values.host}"
-  token = "${values.token}"
-  cluster_ca_certificate = "${values.cluster_ca_certificate}"
+  host                   = var.host
+  token                  = var.token
+  cluster_ca_certificate = var.cluster_ca_certificate
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,21 @@
 variable "project_id" {
-    description = "The ID of the GCP project"
-    type        = string
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "host" {
+  description = "The hostname (in form of URI) of Kubernetes master."
+  type        = string
+}
+
+variable "token" {
+  description = "Bearer token for authentication to the API server."
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_ca_certificate" {
+  description = "PEM-encoded root certificates bundle for TLS authentication."
+  type        = string
+  sensitive   = true
 }
